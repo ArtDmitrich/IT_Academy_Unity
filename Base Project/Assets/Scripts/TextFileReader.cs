dbdb5d66a1d4b040308ee8eb61f41cfe82c5e9bf
ArtDmitrich <klain92@mail.ru>
2024-05-06 19:11:53 +0300

    delete unuseless "using", reconstruct TextFileReader: create static class with static method

--- Base Project/Assets/Scripts/TextFileReader.cs
+++ Base Project/Assets/Scripts/TextFileReader.cs
@@ -1,27 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class TextFileReader : MonoBehaviour
+public static class TextFileReader
 {
-    public static TextFileReader instance = null;
-
     public static string GetTextFromTextFile(string textFileName)
     {
         var textFile = Resources.Load<TextAsset>("Text/" + textFileName);
 
         return textFile.text;
-    }
-
-    private void Start()
-    {
-        if (instance == null)
-        {
-            instance = this;
-        }
-        else
-        {
-            Destroy(gameObject);
-        }    
-    }    
+    }   
 }
